74606395bb5b4f24128a104a4cb58fc38bfcc263
Sergoto <Dklin22@gmail.com>
2022-08-09 14:06:28 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,9 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        yellow_vs: "#D9E577",
-        blue_vs: "#389fdc",
-        lightblue_vs: "#85d8fb",
-        brown_vs: "#d98e73",
-      },
-    },
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
